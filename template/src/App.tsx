1cb0c2e660d51f36aedae700941fccc316ffdeb7
Chloe Danisevicius <chloe.danisevicius@gmail.com>
2023-01-31 09:16:28 +0100

    update template with examples

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,15 +1,81 @@
-import React from 'react';
-import { useCogsConnection, useIsConnected } from '@clockworkdog/cogs-client-react';
-
+import {
+  Hint, Images, Timer, useCogsConfig,
+  useCogsConnection, useCogsEvent, useCogsInputPortValue, useIsConnected, useShowPhase
+} from '@clockworkdog/cogs-client-react';
+import { useCallback, useEffect, useState } from 'react';
 import './App.css';
+import confetti from 'canvas-confetti';
+//import manifestJson from '../../cogs-plugin-manifest.json'
 
-export default function App() {
-  const connection = useCogsConnection();
-  const isConnected = useIsConnected(connection);  
+export default function MediaPlayer() {
+  // For the next version!
+  //const newFancyCogsConnection = useCogsConnection<typeof manifestJson>();
+  const [gameResult, setGameResult] = useState('');
+  const cogsConnection = useCogsConnection<{config: {"Default Background": string}, inputPorts: {Score: number}, inputEvents: {'Game Finished': 'Won' | 'Lost' | 'Finished'}}>();
+  const isConnected = useIsConnected(cogsConnection);
+  const showPhase = useShowPhase(cogsConnection);
+  const searchParams = new URLSearchParams(window.location.search);
+  const clientId = searchParams.get('name');
+
+  const cogsBackgroundColor = useCogsConfig(cogsConnection)["Default Background"];
+  const score = useCogsInputPortValue(cogsConnection, "Score");
+
+  const handleGameFinished = useCallback((value: 'Won' | 'Lost' | 'Finished') => {
+    if (value === "Finished") {
+      setGameResult('');
+    } else if (value === "Won") {
+      setGameResult(value);
+      confetti()
+    } else if (value === "Lost") {
+      setGameResult(value);
+    }
+  }, [])
+
+  useCogsEvent(cogsConnection, 'Game Finished', handleGameFinished)
+  console.log(showPhase);
+
+  useEffect(() => {
+    const originalTitle = document.title;
+    document.title = clientId + ' - COGS';
+    return () => {
+      document.title = originalTitle;
+    };
+  }, [clientId]);
+
+  useEffect(() => {
+    const body = document.body;
+    body.style.background = cogsBackgroundColor;
+  }, [cogsBackgroundColor]);
 
   return (
-  	<div className="App">
-  	  <div>Connected: {isConnected.toString()}</div>
-  	</div>
+    <>
+      {(!isConnected) && (
+        <div id="connection-status" className={isConnected ? 'connected' : ''}>
+          Connecting...
+        </div>
+      )}
+      <div className='container'>
+        <section>
+          <p>Timer</p>
+          <Timer  connection={cogsConnection} />
+        </section>
+        <section>
+          <p>Score</p>
+          <p>{score}</p>
+        </section>
+      </div>
+      {showPhase === "in progress" && (
+        <article>
+          {gameResult === "Won" && <p>You won! Congrats!! 🎉</p>}
+          {gameResult === "Lost" && (
+            <div className="dark-souls">
+              <p>You Lost</p>
+            </div>
+          )}
+        </article>
+      )}
+      <Hint  connection={cogsConnection} />
+      <Images connection={cogsConnection} fullscreen />
+    </>
   );
 }
