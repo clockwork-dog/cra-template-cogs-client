abf4ee94a312acfbcd1032bddab796e232cc96f8
Tom Dixon <tckdixon@gmail.com>
2024-02-02 15:43:26 +0000

    Fix for postinstall symlink step

--- template/scripts/createManifestSymlink.js
+++ template/scripts/createManifestSymlink.js
@@ -2,23 +2,31 @@
 const fs = require("fs");
 const path = require("path");
 
-const originalFile = path.join(__dirname, "..", "src", "cogs-plugin-manifest.js");
-const symlinkToCreate = path.join("..", "public", "cogs-plugin-manifest.js");
+const originalFile = path.join("..", "src", "cogs-plugin-manifest.js");
+const symlinkToCreate = path.join(
+  __dirname,
+  "..",
+  "public",
+  "cogs-plugin-manifest.js"
+);
 
-// On Windows we need to use an absolute path to create the "junction" link as a non-admin user
-// so we create a junction using the absolute path
-if (process.platform === "win32") {
-  fs.link(originalFile, path.resolve(__dirname, symlinkToCreate), (error) => {
-    if (error) {
-      throw error;
-    }
-    console.log("Created hard link:", originalFile, "->", symlinkToCreate);
-  });
-} else {
-  fs.symlink(originalFile, symlinkToCreate, (error) => {
-    if (error) {
-      throw error;
-    }
-    console.log("Created symlink:", originalFile, "->", symlinkToCreate);
-  });
+// If the symlink doesn't exist, create it
+if (!fs.existsSync(symlinkToCreate)) {
+  // On Windows we need to use an absolute path to create the "junction" link as a non-admin user
+  // so we create a junction using the absolute path
+  if (process.platform === "win32") {
+    fs.link(path.resolve(__dirname, originalFile), symlinkToCreate, (error) => {
+      if (error) {
+        throw error;
+      }
+      console.log("Created hard link:", originalFile, "->", symlinkToCreate);
+    });
+  } else {
+    fs.symlink(originalFile, symlinkToCreate, (error) => {
+      if (error) {
+        throw error;
+      }
+      console.log("Created symlink:", originalFile, "->", symlinkToCreate);
+    });
+  }
 }
